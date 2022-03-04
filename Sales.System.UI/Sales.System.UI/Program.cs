6ee07ba99a43b3a6150033f18dfb20baec0d8693
emanuelnascimento1 <emanueel.ns@gmail.com>
2022-03-04 20:50:16 -0300

    Criando Projeto UI

---
+++ Sales.System.UI/Sales.System.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Sales.System.UI.Formularios.MateriaPrima;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sales.System.UI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmCadastrarMateriaPrima());
+        }
+    }
+}
